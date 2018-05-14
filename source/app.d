e00e3d1b1b0d387b473c53d645588e270103c038
Reece Jones <reece_jones@icloud.com>
2018-05-14 13:23:34 -0700

    it now works, but there is still more to do

--- source/app.d
+++ source/app.d
@@ -3,10 +3,12 @@ import vibe.d;
 import pubg.request;
 import service.query;
 import std.array: split;
+import std.string: strip;
+import std.conv: text;
 
 void handleRetard(HTTPServerRequest req, HTTPServerResponse res)
 {
-    auto uri = req.requestURI;
+    auto uri = req.requestURI; 
 	//remove the blank
     string[] s = uri.split("/")[1..$];
     string requestedUser = s[0];
@@ -14,15 +16,41 @@ void handleRetard(HTTPServerRequest req, HTTPServerResponse res)
 	writeln("user: ", requestedUser);
 	writeln("statistic: ", requestedStat);
 	// auto stats = getFullStats("pc-na", "account.f80823e2cd624fe8a5a0aa1899ffcc41");
-	auto stats = getFullStats("pc-na", requestedUser);
-    res.render!("index.dt", stats, requestedStat);
+	// auto stats = getFullStats("pc-na", requestedUser);
+    res.render!("index.dt", requestedStat, requestedUser);
+}
+
+
+void handleWebSocketConnection(scope WebSocket socket)
+{
+	logInfo("Got new web socket connection.");
+	auto user = socket.receiveText().strip;
+	auto stat = socket.receiveText().strip;
+	while (true)
+	{
+		auto stats = getFullStats("pc-na", user);
+		if (!socket.connected) break;
+		string response = "";
+		if (stat == "kills" || stat == "all")
+			response ~= "Kills: " ~ text!int(stats.kills) ~ "<br>";
+		if (stat == "wins" || stat == "all")
+			response ~= "Wins: " ~ text!int(stats.wins) ~ "<br>";
+		if (stat == "losses" || stat == "all")
+			response ~= "Losses: " ~ text!int(stats.losses) ~ "<br>";
+		socket.send(response);
+		//we can wait a while
+		sleep(5.seconds);
+	}
+	logInfo("Client disconnected.");
 }
 
 shared static this()
 {
 	auto router = new URLRouter;
 
-	router.get("*", &handleRetard);
+	router.get("/account.*", &handleRetard);
+	router.get("/ws", handleWebSockets(&handleWebSocketConnection));
+	router.get("*", serveStaticFiles("public/"));
 
 	auto settings = new HTTPServerSettings;
 
