5df0d9a9381d6a874f1f5e8a21316249578dd6a9
Reece Jones <reece_jones@icloud.com>
2018-05-13 19:37:40 -0700

    stuff kinda works now

--- source/app.d
+++ source/app.d
@@ -1,15 +1,34 @@
 import std.stdio;
 import vibe.d;
+import pubg.request;
+import service.query;
+import std.array: split;
+
+void handleRetard(HTTPServerRequest req, HTTPServerResponse res)
+{
+    auto uri = req.requestURI;
+	//remove the blank
+    string[] s = uri.split("/")[1..$];
+    string requestedUser = s[0];
+    string requestedStat = s[1];
+	writeln("user: ", requestedUser);
+	writeln("statistic: ", requestedStat);
+	// auto stats = getFullStats("pc-na", "account.f80823e2cd624fe8a5a0aa1899ffcc41");
+	auto stats = getFullStats("pc-na", requestedUser);
+    res.render!("index.dt", stats, requestedStat);
+}
 
 shared static this()
 {
 	auto router = new URLRouter;
 
-	router.get("*", serveStaticFiles("public/"));
+	router.get("*", &handleRetard);
 
 	auto settings = new HTTPServerSettings;
 
 	settings.port = 8080;
 	settings.bindAddresses = ["::1", "0.0.0.0"];
+
+	setAPIKey("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJkZDUzNTMzMC0zMmYxLTAxMzYtODI3Ny0wOWZlNWVjZTk5YjYiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTI1NTY2MzEyLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImFtb3VudC1vZi1raWxscy1pbi10aW1lLXBlcmlvZCIsInNjb3BlIjoiY29tbXVuaXR5IiwibGltaXQiOjEwfQ.OZCRQlb2Ah2ZkQoo1ImkBNHy0xytQgBgttp_nXZLZqk");
 	listenHTTP(settings, router);
 }
---
+++ source/service/query.d
@@ -0,0 +1,29 @@
+module service.query;
+import pubg.player;
+import service.qdefs;
+import std.stdio;
+
+struct PlayerStats
+{
+    int kills;
+    int headshots;
+    int wins;
+    int losses;
+}
+
+PlayerStats getFullStats(string region, string accountId)
+{
+    PlayerStats playerStats = cast(PlayerStats)0;
+    PlayerRequest playerRequest = new PlayerRequest(region);
+    Player player = playerRequest.getExtendedPlayer(accountId, getLatestSeason());
+    foreach (mode; getGameModes())
+    {
+        auto gmStats = player.getGameModeStats(mode);
+        playerStats.kills += gmStats.getKills();
+        //doesn't work because of spelling error :/
+        //playerStats.headshots += gmStats.getHeadshotKills();
+        playerStats.wins += gmStats.getWins();
+        playerStats.losses += gmStats.getLosses();
+    }
+    return playerStats;
+}
